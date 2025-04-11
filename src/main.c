f4839c3885b275bff5031c93c50f7757b81360b1
Matthew Berhalter <mberhalter01@gmail.com>
2025-04-11 00:48:19 -0400

    added reference info. created main.c cpu.c cpu.h global.h cpu_test.c. wrote boilerplate.

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "../include/global.h"
+
+int main(int argc, char const *argv[]) {
+    (void) argc;
+    (void) argv;
+    return EXIT_SUCCESS;
+}
---
+++ tests/cpu_test.c
@@ -0,0 +1,6 @@
+#include <criterion/criterion.h>
+#include "../include/cpu.h"
+
+Test(cpu, test_valid) {
+    cr_assert_not_null(NULL, "Not implemented.");
+}
