b561e125509ace3a4b637b7bafc3991cb7d6e6b6
Matthew Berhalter <mberhalter01@gmail.com>
2025-04-13 01:39:14 -0400

    added missing stack pointer variable to cpu.h. renamed some members of struct cpu for legibility. added additional resources to important_links.txt

--- include/cpu.h
+++ include/cpu.h
@@ -2,8 +2,9 @@
 
 typedef struct cpu {
     uint16_t index; /* address/index register */
-    uint16_t pc;
+    uint16_t program_ct;
     uint16_t stack[16]; /* 16 2-byte members is the de facto minimum size of the stack; can be expanded if needed. */
+    uint8_t stack_ptr;
     uint8_t registers[16]; /* reg VY can be accessed as registers[0xY] */
     uint8_t ram[4096];
     uint8_t delay_timer;
